7601ceeef2a3fa9e0ee9ddeda1b749b5cd81cd72
John <jkforsythe@hotmail.com>
2019-12-05 19:38:11 +0000

    profile data created

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: male,
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/portraits/men//82.jpg'
+  },
+  {
+    name: 'Jenny Smith',
+    age: 26,
+    gender: female,
+    lookingfor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/portraits/women//82.jpg'
+  },
+  {
+    name: 'Will Johnson',
+    age: 38,
+    gender: male,
+    lookingfor: 'female',
+    location: 'New York NJ',
+    image: 'https://randomuser.me/portraits/men//84.jpg'
+  }
+];
